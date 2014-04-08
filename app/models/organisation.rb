730207084d90fb6010a369f9f43b120709ab4563
Alexandra Goldmann <alexandra.goldmann@gmx.de>
2014-04-08 20:43:48 +0200

    form of address done

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,6 +1,10 @@
 class Organisation < ActiveRecord::Base
 	#belongs_to :nace_code
 
+	FORMS_OF_ADDRESS = ["Herr", "Frau"]
+
 	validates :organisation_name, :organisation_street, :organisation_street_number, :organisation_zipcode, :organisation_city, :organisation_country, :user_form_of_address, :user_first_name, :user_last_name, :user_birthday, :user_phone, :user_email, presence: true
 	# noch mehr validations einfügen, z.B. korrektes datum, korrektes emailformat usw.
+
+	validates :user_form_of_address, inclusion: FORMS_OF_ADDRESS
 end
