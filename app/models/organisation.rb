8b635104285934deee40be4c783ac15084c90388
Alexandra Goldmann <alexandra.goldmann@gmx.de>
2014-04-11 17:23:17 +0200

    some more validation done

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -6,11 +6,32 @@ class Organisation < ActiveRecord::Base
 	COUNTRIES = ["Belgien", "Dänemark", "Deutschland", "Frankreich", "Luxemburg", "Niederlande", "Österreich", "Polen",
 				"Schweiz", "Tschechien"]
 
-	validates :organisation_name, :organisation_street, :organisation_street_number, :organisation_zipcode, :organisation_city, :organisation_country, :user_form_of_address, :user_first_name, :user_last_name, :user_birthday, :user_phone, :user_email, presence: true
-	# noch mehr validations einfügen, z.B. korrektes datum, korrektes emailformat usw.
+	HUMANIZED_ATTRIBUTES = {
+    :organisation_name => "Das Feld 'Name der Organisation'",
+    :organisation_street => "Das Feld 'Straße'",
+    :organisation_street_number => "Das Feld 'Hausnummer'",
+    :organisation_zipcode => "Das Feld 'Postleitzahl'",
+    :organisation_city => "Das Feld 'Stadt'",
+    :organisation_country => "Das Feld 'Land'",
+    :user_form_of_address => "Das Feld 'Anrede'",
+    :user_first_name => "Das Feld 'Vorname'",
+    :user_last_name => "Das Feld 'Nachname'",
+    :user_birthday => "Das Feld 'Geburtsdatum'",
+    :user_phone => "Das Feld 'Telefon'",
+    :user_email => "Das Feld 'E-Mail-Adresse'"
+    }
 
-	validates :organisation_country, inclusion: COUNTRIES
+    def self.human_attribute_name(attr, options={})
+     HUMANIZED_ATTRIBUTES[attr.to_sym] || super
+    end
 
-	validates :user_form_of_address, inclusion: FORMS_OF_ADDRESS
+	validates :organisation_name, :organisation_street, :organisation_street_number, :organisation_zipcode,
+	:organisation_city, :user_first_name, :user_last_name, :user_birthday, :user_phone, :user_email,
+	presence: {message: "%{value} darf nicht leer sein."} 
+	validates :organisation_country, inclusion: {in: COUNTRIES, message: "wurde nicht ausgewählt."}
+	validates :user_form_of_address, inclusion: {in: FORMS_OF_ADDRESS, message: "wurde nicht ausgewählt."}
+	validates :user_email, uniqueness: true
+
+	#validate birthday and email
 
 end
